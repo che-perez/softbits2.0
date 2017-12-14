bb33eb3dd63aec4e20694ed41609be51bc2551d4
Josue Perez <che.perez3@gmail.com>
2017-12-14 15:53:19 -0500

    create kiosks controller

---
+++ POSapp/app/controllers/kiosks_controller.rb
@@ -0,0 +1,32 @@
+class KiosksController < ApiController
+	before_action :require_login
+	
+	def index
+	  kiosks = Kiosk.all
+	  render json: { kiosks: kiosks } 
+	end
+	
+	def show
+	 kiosk = Kiosk.find(params[:id])
+	 kiosk_user = kiosk.user
+	 render json: { kiosk: kiosk, username: kiosk_user.username }
+	end
+	
+	def create
+	 kiosk = Kiosk.new(kiosk_params)
+	 kiosk.user = current_user
+	 
+	 if kiosk.save
+		 render json: { message: 'ok', kiosk: kiosk, }
+	 else
+	 	render json:{ message: 'Could not create Kiosk' }
+	 end
+	end
+	
+	private
+	
+	def kiosk_params
+	  params.require(:kiosk).permit(:kiosk_name, :location, :address)
+	end
+	  
+end
--- POSapp/app/models/kiosk.rb
+++ POSapp/app/models/kiosk.rb
@@ -1,4 +1,5 @@
 class Kiosk < ApplicationRecord
 	
 	belongs_to :user
+	has_many :inventories
 end
